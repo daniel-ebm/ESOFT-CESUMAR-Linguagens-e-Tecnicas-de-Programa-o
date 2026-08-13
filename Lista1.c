80c54c5bea33c13767adca744704de6910b2b29b
Daniel Eberharte Mauloni <daniel.eberharte@gmail.com>
2026-08-13 11:39:10 -0300

    Add files via upload
    claramente nao sei o que estou fazendo

---
+++ Lista1.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+/*funções do math.h 
+
+
+*/
+
+int main(int argc, char *argv[]) {
+	
+	int primeiro, segundo, aux;
+	/*
+	printf("Insira o primeiro numero: ");
+	scanf("%d", &primeiro);
+	printf("Insira o segundo numero: ");
+	scanf("%d", &segundo);
+		
+		aux = primeiro;
+		primeiro = segundo;
+		segundo = aux;
+	
+	printf("%d \n %d", primeiro, segundo);
+	*/
+// quarto exercicio
+	
+	int fixo, vendas, comissao, total;
+	printf("Qual seu salario fixo: ");
+	scanf("%d", &fixo);
+	printf("Quanto vendeu: ");
+	scanf("%d", &vendas);
+	comissao = (vendas * 15) /100
+	printf("O salario total é: fixo + comissao ");
+	
+		return 0;
+}
