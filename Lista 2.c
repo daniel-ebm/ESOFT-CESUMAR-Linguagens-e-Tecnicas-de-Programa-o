8062f88ee8ad250d20c7413c9637032e5da5a2a2
Daniel Eberharte Mauloni <daniel.eberharte@gmail.com>
2026-09-02 05:12:55 -0300

    antes tarde do que nunca

--- Lista 2.c
+++ Lista 2.c
@@ -2,6 +2,136 @@
 #include <stdlib.h>
 
 int main(int argc, char *argv[]) {
+
+    printf("=== EXERCICIO 1 ===\n");
+
+    int idade, anoAtual, anoNascimento;
+
+    printf("Informe sua idade: ");
+    scanf("%d", &idade);
+
+    printf("Informe o ano atual: ");
+    scanf("%d", &anoAtual);
+
+    anoNascimento = anoAtual - idade;
+
+    printf("Ano de nascimento: %d\n\n", anoNascimento);
+
+
+    printf("=== EXERCICIO 2 ===\n");
+
+    double velocidade, velocidadeMS;
+
+    printf("Informe a velocidade em km/h: ");
+    scanf("%lf", &velocidade);
+
+    velocidadeMS = velocidade / 3.6;
+
+    printf("Velocidade em m/s: %.2f\n\n", velocidadeMS);
+
+
+    printf("=== EXERCICIO 3 ===\n");
+
+    double reais, cotacao, dolares;
+
+    printf("Informe o valor em reais: ");
+    scanf("%lf", &reais);
+
+    printf("Informe a cotacao do dolar: ");
+    scanf("%lf", &cotacao);
+
+    dolares = reais / cotacao;
+
+    printf("Valor em dolares: %.2f\n\n", dolares);
+
+
+    printf("=== EXERCICIO 4 ===\n");
+
+    double celsius, fahrenheit;
+
+    printf("Informe a temperatura em Celsius: ");
+    scanf("%lf", &celsius);
+
+    fahrenheit = celsius * (9.0 / 5.0) + 32;
+
+    printf("Temperatura em Fahrenheit: %.2f\n\n", fahrenheit);
+
+
+    printf("=== EXERCICIO 5 ===\n");
+
+    double graus, radianos;
+
+    printf("Informe o angulo em graus: ");
+    scanf("%lf", &graus);
+
+    radianos = graus * 3.141592 / 180;
+
+    printf("Angulo em radianos: %.4f\n\n", radianos);
+
+
+    printf("=== EXERCICIO 6 ===\n");
+
+    int numero, antecessor, sucessor;
+
+    printf("Informe um numero inteiro: ");
+    scanf("%d", &numero);
+
+    antecessor = numero - 1;
+    sucessor = numero + 1;
+
+    printf("Antecessor: %d\n", antecessor);
+    printf("Sucessor: %d\n\n", sucessor);
+
+
+    printf("=== EXERCICIO 7 ===\n");
+
+    double premio, primeiro, segundo, terceiro;
+
+    premio = 780000.00;
+
+    primeiro = premio * 0.46;
+    segundo = premio * 0.32;
+    terceiro = premio - primeiro - segundo;
+
+    printf("Primeiro ganhador: %.2f\n", primeiro);
+    printf("Segundo ganhador: %.2f\n", segundo);
+    printf("Terceiro ganhador: %.2f\n\n", terceiro);
+
+
+    printf("=== EXERCICIO 8 ===\n");
+
+    int segundos, horas, minutos, segundosRestantes;
+
+    printf("Informe o tempo em segundos: ");
+    scanf("%d", &segundos);
+
+    horas = segundos / 3600;
+    segundosRestantes = segundos % 3600;
+    minutos = segundosRestantes / 60;
+    segundosRestantes = segundosRestantes % 60;
+
+    printf("%d horas, %d minutos e %d segundos\n\n", horas, minutos, segundosRestantes);
+
+
+    printf("=== EXERCICIO 9 ===\n");
+
+    double tempo, velocidadeMedia, distancia, consumo, litros;
+
+    printf("Informe o tempo da viagem em horas: ");
+    scanf("%lf", &tempo);
+
+    printf("Informe a velocidade media em km/h: ");
+    scanf("%lf", &velocidadeMedia);
+
+    printf("Informe o consumo do veiculo em km/l: ");
+    scanf("%lf", &consumo);
+
+    distancia = tempo * velocidadeMedia;
+    litros = distancia / consumo;
+
+    printf("Distancia percorrida: %.2f km\n", distancia);
+    printf("Litros necessarios: %.2f litros\n\n", litros);
+
 /*
 	printf("=== EXERCICIO 10 ===\n");
 	
--- Lista1.c
+++ Lista1.c
@@ -1,15 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-/*funções do math.h 
-
-
-*/
 
 int main(int argc, char *argv[]) {
 	
 	int primeiro, segundo, aux;
-	/*
+
+	 printf("=== EXERCICIO 1 ===\n");
+	
 	printf("Insira o primeiro numero: ");
 	scanf("%d", &primeiro);
 	printf("Insira o segundo numero: ");
@@ -19,17 +17,133 @@ int main(int argc, char *argv[]) {
 		primeiro = segundo;
 		segundo = aux;
 	
-	printf("%d \n %d", primeiro, segundo);
-	*/
-// quarto exercicio
+	printf("%d\n%d\n\n", primeiro, segundo);
 	
-	int fixo, vendas, comissao, total;
-	printf("Qual seu salario fixo: ");
-	scanf("%d", &fixo);
-	printf("Quanto vendeu: ");
-	scanf("%d", &vendas);
-	comissao = (vendas * 15) /100
-	printf("O salario total é: fixo + comissao ");
+	 printf("=== EXERCICIO 2 ===\n");
+
+    double numero, A;
+    int N;
+
+    printf("Insira o valor: ");
+    scanf("%lf", &numero);
+
+    A = numero;
+    N = 0;
+
+    while (A >= 10) {
+        A /= 10;
+        N++;
+    }
+
+    while (A < 1) {
+        A *= 10;
+        N--;
+    }
+
+    printf("%.2f x 10^%d\n\n", A, N);
+
+
+    printf("=== EXERCICIO 3 ===\n");
+
+    int bit1, bit2, bit3, bit4, bit5, bit6, bit7, bitx, n;
+
+    printf("Insira o valor para ser convertido: ");
+    scanf("%d", &bitx);
+
+    n = bitx;
+
+    bit7 = bitx % 2;
+    bitx /= 2;
+
+    bit6 = bitx % 2;
+    bitx /= 2;
+
+    bit5 = bitx % 2;
+    bitx /= 2;
+
+    bit4 = bitx % 2;
+    bitx /= 2;
+
+    bit3 = bitx % 2;
+    bitx /= 2;
+
+    bit2 = bitx % 2;
+    bitx /= 2;
+
+    bit1 = bitx % 2;
+    bitx /= 2;
+
+    printf("O numero %d em binario: %d%d%d%d%d%d%d\n\n", n, bit1, bit2, bit3, bit4, bit5, bit6, bit7);
+
+    printf("=== EXERCICIO 4 ===\n");
+
+    double salario, vendas, comissao, total;
+
+    printf("Informe o salario fixo: ");
+    scanf("%lf", &salario);
+
+    printf("Informe o valor total em vendas: ");
+    scanf("%lf", &vendas);
+
+    comissao = vendas * 0.15;
+    total = salario + comissao;
+
+    printf("Total a receber: %.2f\n\n", total);
+
+    printf("=== EXERCICIO 5 ===\n");
+
+    double a, b, c, d, soma, media, produto;
+
+    printf("Informe quatro valores: ");
+    scanf("%lf %lf %lf %lf", &a, &b, &c, &d);
+
+    soma = a + b + c + d;
+    media = soma / 4;
+    produto = a * b * c * d;
+
+    printf("Soma: %.2f\n", soma);
+    printf("Media: %.2f\n", media);
+    printf("Produto: %.2f\n\n", produto);
+
+    printf("=== EXERCICIO 6 ===\n");
+
+    int idadeDias, anos, meses, dias, restante;
+
+    printf("Informe a idade em dias: ");
+    scanf("%d", &idadeDias);
+
+    anos = idadeDias / 365;
+    restante = idadeDias % 365;
+    meses = restante / 30;
+    dias = restante % 30;
+
+    printf("%d anos, %d meses e %d dias\n\n", anos, meses, dias);
+
+    printf("=== EXERCICIO 7 ===\n");
+
+    double raio, volume;
+
+    printf("Informe o raio da esfera: ");
+    scanf("%lf", &raio);
+
+    volume = (4.0 / 3.0) * 3.14159 * pow(raio, 3);
+
+    printf("Volume da esfera: %.4f\n\n", volume);
+
+	printf("=== EXERCICIO 8  ===\n");
+	int x1, x2, y1, y2, v1, v2;
+	float distancia;
+	printf("Insira as coordenadas do ponto P1: ");
+	scanf("%d %d", &x1,&y1);
 	
+	printf("Insira as coordenadas do ponto P2: ");
+	scanf("%d %d", &x2,&y2);
+	
+	v1 = pow(x2-x1, 2);
+	v2 = pow(y2-y1, 2);
+	distancia = sqrt(v1+v2);
+	
+	printf("Distancia (%f)",distancia);
+
 		return 0;
 }
