037d539b28af8047954f4a6af35a6b1e23780578
Daniel Eberharte Mauloni <daniel.eberharte@gmail.com>
2026-09-14 20:03:50 -0300

    tirando espacos na resosta

--- aula de hoje.c
+++ aula de hoje.c
@@ -1,32 +1,51 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int multDigito( int dig, int valor){
-	return dig*valor;
+int multDigito(int dig, int valor){
+	return  dig*valor;
 }
 
+
+
 int main(int argc, char *argv[]) {
-	int dig1, dig2, dig3, diig4, dig5, dig6, dig7, dig8, dig9, digv1, digv2, soma, resto;
-	scanf("%d %d %d . %d %d %d . %d %d %d - %d %d",
-	&dig1, &dig2, &dig3, &dig4, &dig5, &dig6, &dig7, &dig8, &dig9, &digv1, &digv2);
 	
-	printf(" Confirma o cpf %d%d%d%.d%d%d.%d%d%d-%d%d",
-		dig1, dig2, dig3, diig4, dig5, dig6, dig7, dig8, dig9, digv1, digv2);	
-	soma = multDigito(dg1,10)+multDigito(dig2,9)+multDigito(dig3,8)
-	+multDigito(dig4,7)+multDigito(dig5,6)+multDigito(dig6,5)+
-	multDigito(dig7,4)+multDigito(dig8,3)+multDigito(dig9+3)+multDigito(dig9,2);
+	int dg1,dg2,dg3,dg4,dg5,dg6,dg7,dg8,dg9,dgv,dgv2,soma,restoI,restoII;
+                                                                                
+	printf("Insira seu cpf: ");
+	scanf("%1d%1d%1d.%1d%1d%1d.%1d%1d%1d-%1d%1d"
+			,&dg1,&dg2,&dg3,&dg4,&dg5,&dg6,&dg7,&dg8,&dg9,&dgv,&dgv2);
 	
-	soma*=10;
-	resto = soma%11;
-	if (resto == 10) resto == 0;
-	printf("\n%d",resto);
+	printf("\nCONFIRME O CPF: %d%d%d.%d%d%d.%d%d%d-%d%d"
+			,dg1,dg2,dg3,dg4,dg5,dg6,dg7,dg8,dg9,dgv,dgv2);
 	
-	soma = multDigito(dg1+11)+multDigito(dig2,10)+multDigito(dig3,9)+
-		multDigito(dig4,8)+multDigito(dig8,4)+multDigito(dig,3)+multDigito(dgv,2);
-		
+	soma = multDigito(dg1,10)+ multDigito(dg2,9)+multDigito(dg3,8)+
+			multDigito(dg4,7)+multDigito(dg5,6)+multDigito(dg6,5)+
+			multDigito(dg7,4)+multDigito(dg8,3)+multDigito(dg9,2);
+	soma *=10;
+	restoI = soma%11;
+	if(restoI == 10){
+		restoI = 0;
+	}
+	printf("\n%d",restoI);
+	
+	
+	
+	soma = multDigito(dg1,11)+ multDigito(dg2,10)+multDigito(dg3,9)+
+			multDigito(dg4,8)+multDigito(dg5,7)+multDigito(dg6,6)+
+			multDigito(dg7,5)+multDigito(dg8,4)+multDigito(dg9,3)+multDigito(restoI,2);
 	soma *=10;
 	restoII = soma%11;
-	if (restoII == 10) resto = 0;
-	printf("\nd", restoII);
-	return 0;
+	if(restoII == 10){
+		restoII = 0;
+	}
+	printf("\n%d",restoII);
+	
+	if(restoI == dgv && restoII == dgv2){
+		printf("\nCPF VALIDO");
+	}else{
+		printf("\nCPF INVALIDO");
+	}
+	
+	
+return 0;
 }
